032b0b4ea04cb3399e6af761c5d827d49a2870c8
Peter Nguyen <petertdnguyen@outlook.com>
2019-03-03 16:43:54 -0800

    added functionality to view stock price for non-comparative API call

--- routes/api.js
+++ routes/api.js
@@ -21,28 +21,49 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       //Get price of only one stock
-      const firstStock = req.query.singleStock;
-      //Get price of both stock
-      const firstOfTwoStocks = req.query.firstDoubleStock;
-      const secondOfTwoStocks = req.query.secondOfTwoStocks;
+      const firstStock = req.query.stock;
+      const secondStock = req.query.stock;
       //Capture stock info
-      let stockData;
+      let firstStockPrice;
+      let secondStockPrice;
 
       //create API request url
-      const apiKey = 'VSV71O3KBKXN8CRH';
-      const dateToday = moment().format("YYYY-MM-DD");
+      const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
+      let dateToday = moment();
 
       request.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + firstStock + '&outputsize=compact&apikey=' + apiKey, 
       (err, response, body) => {
         if (err) {
           return next(err);
         }
+
+        //Stock markets do not update on weekends. To get the latest price, you need closing date of most recent Friday.
+        switch(dateToday.day()) {
+          case 0: //Sunday. Get Friday's date
+            dateToday = dateToday.subtract(2, "days");
+            break;
+          case 1:
+            break;
+          case 2:
+            break;
+          case 3:
+            break;
+          case 4:
+            break;
+          case 5:
+            break;
+          case 6:
+            dateToday = dateToday.subtract(1, "days"); //Saturday. Get Friday's date
+        }
+
         const rawInfo = JSON.parse(body);
-        stockData = (rawInfo["Time Series (Daily)"]);
-        console.log(stockData);
+        firstStockPrice = (rawInfo["Time Series (Daily)"][dateToday.format("YYYY-MM-DD")]["4. close"]);
+
+        //Send stock price info
+
+        //{"stockData":{"stock":"GOOG","price":"786.90","likes":1}}
+        res.send({ "stockData": { "stock": firstStock.toUpperCase(), "price": firstStockPrice, "likes": 1 } });
       })
-      //Send stock price info
-      // res.send(stockData);
     });
 
 };
